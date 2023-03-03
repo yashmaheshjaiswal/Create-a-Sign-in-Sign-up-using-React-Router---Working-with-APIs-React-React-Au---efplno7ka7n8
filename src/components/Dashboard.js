f1abdb8159516e7025e30a8da91915639c93df21
Yash Jaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-03-03 22:33:20 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 
 function Dashboard({ setLoggedIn }) {
 
     return (
         <div id="dashboardPage">
             <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
+            <Link to="/"><button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button></Link>
         </div>
     )
 
